ceaeae084f0fa7bacee5690cd309c98e50efec3a
mstfsu <mustafasu363636@hotmail.com>
2023-02-22 00:22:10 +0300

    code format

--- IHA/tests.py
+++ IHA/tests.py
@@ -13,14 +13,15 @@ class IHATestCase(TestCase):
         self.brand = Brand.objects.create(name="test")
         self.user = User.objects.create_user(username="test", password="test")
         self.client.force_login(self.user)
+
     def test_view_set(self):
         """ test all actions of drf viewset with client"""
-        #factory = APIRequestFactory()
+
         request = self.client.post('/iha/', {'name': 'deneme', 'model_number': '123', 'description': 'test',
-                               'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id})
-        #request = IHAViewSet.as_view({'post': 'create'})(request)
+                                             'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id})
+
         self.assertEqual(request.status_code, 201)
-        
+
         request = self.client.get('/iha/')
         self.assertEqual(request.status_code, 200)
 
@@ -28,9 +29,9 @@ class IHATestCase(TestCase):
         self.assertEqual(request.status_code, 200)
 
         request = self.client.put('/iha/1/', {'name': 'aa', 'model_number': '44', 'description': 'test',
-                              'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id}, content_type='application/json')
+                                              'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id}, content_type='application/json')
         self.assertEqual(request.status_code, 200)
-       
+
         IHA.objects.last()
         self.assertEqual(IHA.objects.last().name, 'aa')
 
@@ -38,4 +39,3 @@ class IHATestCase(TestCase):
         self.assertEqual(request.status_code, 204)
 
         self.assertEqual(IHA.objects.count(), 0)
-        
