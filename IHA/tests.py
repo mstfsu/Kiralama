d06aa3c181fd059ee2dc069be25cdb9a4f49e24b
mstfsu <mustafasu363636@hotmail.com>
2023-02-22 00:21:30 +0300

    unit test client

--- IHA/tests.py
+++ IHA/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
 from IHA.views import IHAViewSet
 from rest_framework.test import APIRequestFactory
 from .models import *
@@ -9,34 +11,31 @@ class IHATestCase(TestCase):
     def setUp(self):
         self.category = Category.objects.create(name="test")
         self.brand = Brand.objects.create(name="test")
-
+        self.user = User.objects.create_user(username="test", password="test")
+        self.client.force_login(self.user)
     def test_view_set(self):
-        """ test all actions of drf viewset with RequestFactory"""
-        factory = APIRequestFactory()
-        request = factory.post('/iha/', {'name': 'deneme', 'model_number': '123', 'description': 'test',
+        """ test all actions of drf viewset with client"""
+        #factory = APIRequestFactory()
+        request = self.client.post('/iha/', {'name': 'deneme', 'model_number': '123', 'description': 'test',
                                'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id})
-        response = IHAViewSet.as_view({'post': 'create'})(request)
-        self.assertEqual(response.status_code, 201)
-    
-        request = factory.get('/iha/')
-        response = IHAViewSet.as_view({'get': 'list'})(request)
-        self.assertEqual(response.status_code, 200)
+        #request = IHAViewSet.as_view({'post': 'create'})(request)
+        self.assertEqual(request.status_code, 201)
+        
+        request = self.client.get('/iha/')
+        self.assertEqual(request.status_code, 200)
 
-        request = factory.get('/iha/1/')
-        response = IHAViewSet.as_view({'get': 'retrieve'})(request, pk=1)
-        self.assertEqual(response.status_code, 200)
+        request = self.client.get('/iha/1/')
+        self.assertEqual(request.status_code, 200)
 
-        request = factory.put('/iha/1/', {'name': 'aa', 'model_number': '44', 'description': 'test',
-                              'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id})
-        response = IHAViewSet.as_view({'put': 'update'})(request, pk=1)
-        self.assertEqual(response.status_code, 200)
+        request = self.client.put('/iha/1/', {'name': 'aa', 'model_number': '44', 'description': 'test',
+                              'weight': 12.2, 'category': [self.category.id], 'brand': self.brand.id}, content_type='application/json')
+        self.assertEqual(request.status_code, 200)
        
         IHA.objects.last()
         self.assertEqual(IHA.objects.last().name, 'aa')
 
-        request = factory.delete('/iha/1/')
-        response = IHAViewSet.as_view({'delete': 'destroy'})(request, pk=1)
-        self.assertEqual(response.status_code, 204)
+        request = self.client.delete('/iha/1/')
+        self.assertEqual(request.status_code, 204)
 
         self.assertEqual(IHA.objects.count(), 0)
         
