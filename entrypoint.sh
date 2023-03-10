26426a0c18985c8f248efbdc3f15593e3a6a459b
mstfsu <mustafasu363636@hotmail.com>
2023-03-10 17:02:51 +0300

    try commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ then
 fi
 cd /home/app/backend
 pip install -r requirements.txt
-
+python manage.py collectstatic --no-input --clear
 python manage.py migrate 
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell
