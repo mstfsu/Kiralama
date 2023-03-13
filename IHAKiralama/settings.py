076cf87260b6f5a35c11014652d19bd7c710ba72
mstfsu <mustafasu363636@hotmail.com>
2023-03-13 14:06:41 +0300

    force

--- IHAKiralama/settings.py
+++ IHAKiralama/settings.py
@@ -22,7 +22,6 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
