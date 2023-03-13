c219d47c125071fb883d52afeb6b5f892d2e311a
mstfsu <mustafasu363636@hotmail.com>
2023-03-13 14:37:10 +0300

    dep

--- server_deploy.sh
+++ server_deploy.sh
@@ -1,16 +1,13 @@
 echo "Deploying application ..."
     git stash save
-    git pull origin master
+    git pull origin try
     # pip install --upgrade pip
     # pip install -r requirements.txt
     # sudo systemctl restart nginx
     # sudo systemctl restart uwsgi
     # sudo supervisorctl stop all
     # sudo supervisorctl start all
-    cp .env.dev .env
     chmod +x entrypoint.sh
     docker restart $(docker ps -a -q)
-    docker stop frontend
-    chmod +x backup_database.sh
     chmod +x server_deploy.sh
 echo "Application deployed!"
